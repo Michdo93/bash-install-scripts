012b632228c938b4098481f2ee0a6b0c03a4dcb6
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-16 14:55:27 +0100

    Create install_isc_dhcp_server.bash

---
+++ install_isc_dhcp_server.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install curl git wget net-tools -y
+
+sudo apt install isc-dhcp-server
+
+sudo systemctl start isc-dhcp-server
+sudo systemctl enable isc-dhcp-server
