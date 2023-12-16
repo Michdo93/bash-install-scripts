15b0ac5c28479c4367cda522878826d0b6c1d34f
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-16 14:56:34 +0100

    Create install_kea_dhcp_server.bash

---
+++ install_kea_dhcp_server.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install curl git wget net-tools -y
+
+sudo apt install kea-dhcp4-server
+
+sudo systemctl start kea-dhcp4-server
+sudo systemctl enable kea-dhcp4-server
