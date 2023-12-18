c086fb175b9afcb7b997500e7bdb92649c4a4f1a
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-18 17:31:09 +0100

    Update install_python.bash

--- install_python.bash
+++ install_python.bash
@@ -7,10 +7,10 @@ sudo apt install curl git wget net-tools -y
 sudo apt install python2 python2-dev -y
 sudo apt-get install python3 python3-dev -y
 
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 0
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
-sudo update-alternatives --config python
+#sudo update-alternatives --config python
 
 curl https://raw.githubusercontent.com/Michdo93/get-pip/main/get-pip.py -o get-pip.py
 python get-pip.py
