ecd1343d248606c42d95dfaafdd5eb6fd9ab5abf
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-18 19:41:19 +0100

    Update install_grafana.bash

--- install_grafana.bash
+++ install_grafana.bash
@@ -3,3 +3,13 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install curl git wget net-tools -y
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo apt update
+sudo apt install -y grafana
+
+# Grafana-Dienst starten und aktivieren
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
