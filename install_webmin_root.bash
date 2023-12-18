4f75f5800aae6e96d646732415a104d41a92055d
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-18 13:12:45 +0100

    Create install_webmin_root.bash

---
+++ install_webmin_root.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+
+apt install curl git wget net-tools -y
+
+sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
+wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
+
+apt update
+apt install webmin
+
+systemctl start webmin
+systemctl enable webmin
