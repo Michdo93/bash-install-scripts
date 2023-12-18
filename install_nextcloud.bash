9af3045c5cb5e5536b09dd3c56f7d9e6f42e471a
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-18 19:03:49 +0100

    Update install_nextcloud.bash

--- install_nextcloud.bash
+++ install_nextcloud.bash
@@ -4,3 +4,20 @@ sudo apt upgrade -y
 
 sudo apt install curl git wget net-tools -y
 
+sudo apt update && sudo apt upgrade
+sudo apt install apache2 mariadb-server libapache2-mod-php php-gd php-mysql \
+php-curl php-mbstring php-intl php-gmp php-bcmath php-xml php-imagick php-zip
+
+./install_mysql.bash
+
+wget https://download.nextcloud.com/server/releases/nextcloud-x.y.z.tar.bz2.asc
+wget https://nextcloud.com/nextcloud.asc
+gpg --import nextcloud.asc
+gpg --verify nextcloud-x.y.z.tar.bz2.asc nextcloud-x.y.z.tar.bz2
+
+tar -xjvf nextcloud-x.y.z.tar.bz2
+unzip nextcloud-x.y.z.zip
+
+sudo cp -r nextcloud /var/www
+
+sudo chown -R www-data:www-data /var/www/nextcloud
