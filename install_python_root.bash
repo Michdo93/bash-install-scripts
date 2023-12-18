01fa08a20f11db813d72933d0cca14d5712775f0
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-18 13:07:51 +0100

    Create install_python_root.bash

---
+++ install_python_root.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+
+apt install curl git wget net-tools -y
+
+apt install python2 python2-dev
+apt-get install python3 python3-dev
+
+update-alternatives --install /usr/bin/python python /usr/bin/python2 1
+update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+
+update-alternatives --config python
+
+curl https://raw.githubusercontent.com/Michdo93/get-pip/main/get-pip.py -o get-pip.py
+python get-pip.py
+
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | python3
