898479cb65e2a848a86c1b8f72a8f3df04a97203
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-18 15:19:04 +0100

    Update install_kea_dhcp_server.bash

--- install_kea_dhcp_server.bash
+++ install_kea_dhcp_server.bash
@@ -4,7 +4,7 @@ sudo apt upgrade -y
 
 sudo apt install curl git wget net-tools -y
 
-sudo apt install kea-dhcp4-server
+sudo apt install kea-dhcp4-server -y
 
 sudo systemctl start kea-dhcp4-server
 sudo systemctl enable kea-dhcp4-server
