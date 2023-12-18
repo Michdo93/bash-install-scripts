b35f9ce89c46b708756c55c425d1e57f11daf66d
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-18 15:41:13 +0100

    Update install_jellyfin.bash

--- install_jellyfin.bash
+++ install_jellyfin.bash
@@ -3,11 +3,13 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install curl git wget net-tools -y
+sudo apt-get install dos2unix
 
 # Jellyfin: Port 8096
 sudo apt install apt-transport-https
 sudo wget -O /usr/share/keyrings/jellyfin-archive-keyring.gpg https://repo.jellyfin.org/ubuntu/jellyfin-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/jellyfin-archive-keyring.gpg] https://repo.jellyfin.org/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+sudo dos2unix /etc/apt/sources.list.d/jellyfin.list
 
 sudo apt update
 sudo apt install jellyfin -y
