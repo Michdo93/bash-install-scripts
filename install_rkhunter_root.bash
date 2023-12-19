97b7c1dcc128e584a0646676a5b4fb234ddfc81e
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:25:17 +0100

    Update install_rkhunter_root.bash

--- install_rkhunter_root.bash
+++ install_rkhunter_root.bash
@@ -4,8 +4,8 @@ apt upgrade -y
 
 apt install curl git wget net-tools -y
 
-apt-get install rkhunter -y
-apt-get install mailutils -y
+apt install rkhunter -y
+apt install mailutils -y
 
 echo '10 3 * * * root /usr/bin/rkhunter --cronjob' | tee -a /etc/crontab
 echo '@reboot root /usr/bin/rkhunter -c' | tee -a /etc/crontab
