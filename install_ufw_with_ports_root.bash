039262b501ca5f1ac44ffd8279a549c1b8ffccf2
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:28:49 +0100

    Update install_ufw_with_ports_root.bash

--- install_ufw_with_ports_root.bash
+++ install_ufw_with_ports_root.bash
@@ -32,7 +32,7 @@ cat ufw_configuration.txt
 sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
 wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
 apt update
-apt install webmin
+apt install webmin -y
 
 systemctl start webmin
 systemctl enable webmin
