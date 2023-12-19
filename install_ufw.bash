fd7b75f4ae7e4ca7f59ecf7b1cbcf38268dd6d06
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:27:49 +0100

    Update install_ufw.bash

--- install_ufw.bash
+++ install_ufw.bash
@@ -4,7 +4,7 @@ sudo apt upgrade -y
 
 sudo apt install curl git wget net-tools -y
 
-sudo apt install ufw
+sudo apt install ufw -y
 
 sudo ufw enable
 sudo ufw allow 80       # Beispiel für Port 80 (HTTP)
@@ -14,11 +14,11 @@ sudo ufw allow 22      # Beispiel für SSH
 sudo sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
 wget -qO - http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
 sudo apt update
-sudo apt install webmin
+sudo apt install webmin -y
 
 sudo systemctl start webmin
 sudo systemctl enable webmin
 
-sudo apt install webmin-ufw
+sudo apt install webmin-ufw -y
 
 sudo ufw allow 10000
