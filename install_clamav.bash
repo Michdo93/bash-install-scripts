939e228195eeb7f52472f3be44572e2a263c0e53
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:11:09 +0100

    Update install_clamav.bash

--- install_clamav.bash
+++ install_clamav.bash
@@ -4,8 +4,8 @@ sudo apt upgrade -y
 
 sudo apt install curl git wget net-tools -y
 
-sudo apt-get install clamav clamav-freshclam -y
-sudo apt-get install clamav-docs -y
+sudo apt install clamav clamav-freshclam -y
+sudo apt install clamav-docs -y
 
 echo '10 3 * * * root /usr/bin/clamscan -ir / | /usr/bin/grep FOUND >> /home/ubuntu/clamavinfected.txt' | sudo tee -a /etc/crontab
 echo '@reboot root /usr/bin/clamscan -ir / | /usr/bin/grep FOUND >> /home/ubuntu/clamavinfected.txt' | sudo tee -a /etc/crontab
