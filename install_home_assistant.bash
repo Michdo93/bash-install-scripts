99f4930f3cdc4d00937037fd194ac2518e269757
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:14:29 +0100

    Update install_home_assistant.bash

--- install_home_assistant.bash
+++ install_home_assistant.bash
@@ -8,8 +8,8 @@ sudo apt install curl git wget net-tools -y
 
 # As root
 sudo su
-apt-get update
-apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+apt update
+apt install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 
 # Switch to Home Assistant User
 sudo su - homeassistant
