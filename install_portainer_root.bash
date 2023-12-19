48ca8eaf42f3c0f72e343c58f15c4f8dfebfbf45
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 21:56:42 +0100

    Update install_portainer_root.bash

--- install_portainer_root.bash
+++ install_portainer_root.bash
@@ -22,11 +22,15 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docke
 
 groupadd docker
 usermod -aG docker $USER
-newgrp docker
 
 systemctl start docker.service
 systemctl enable docker.service
 systemctl start containerd.service
 systemctl enable containerd.service
 
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+# Warten, bis Docker-Dienste vollständig initialisiert sind
+while ! docker info &>/dev/null; do
+    sleep 1
+done
+
+docker run -d -p 9000:9000 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:lates
