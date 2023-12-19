4c306dba9783d513ac1a78bf6f0e60549ae4c9cf
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:23:06 +0100

    Update install_plex.bash

--- install_plex.bash
+++ install_plex.bash
@@ -3,17 +3,11 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install curl git wget net-tools -y
-sudo apt-get install dos2unix
 
 # Plex Port 32400
-#wget https://downloads.plex.tv/plex-media-server-new/1.32.7.7621-871adbd44/debian/plexmediaserver_1.32.7.7621-871adbd44_amd64.deb
-#sudo dpkg -i plexmediaserver_1.32.7.7621-871adbd44_amd64.deb
 
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
-
-# Entfernen von Carriage-Return-Zeichen am Ende der Datei
-sudo dos2unix /etc/apt/sources.list.d/plexmediaserver.list
 
 sudo apt update
 sudo apt install plexmediaserver -y
