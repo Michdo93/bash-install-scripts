7b817deae96671429942e85eabcf0a4a095fc2af
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:19:43 +0100

    Update install_mosquitto_root.bash

--- install_mosquitto_root.bash
+++ install_mosquitto_root.bash
@@ -5,9 +5,9 @@ apt upgrade -y
 apt install curl git wget net-tools -y
 
 apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
-apt-get update
-apt-get install mosquitto -y
-apt-get install mosquitto-clients -y
+apt update
+apt install mosquitto -y
+apt install mosquitto-clients -y
 apt clean
 
 systemctl start mosquitto.service
