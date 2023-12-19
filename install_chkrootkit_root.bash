5fa197c1eed6d564bb843aa15d51b4c7b0fc3649
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:10:52 +0100

    Update install_chkrootkit_root.bash

--- install_chkrootkit_root.bash
+++ install_chkrootkit_root.bash
@@ -4,7 +4,7 @@ apt upgrade -y
 
 apt install curl git wget net-tools -y
 
-apt-get install chkrootkit -y
+apt install chkrootkit -y
 
 echo '10 3 * * * root /usr/bin/chkrootkit' | tee -a /etc/crontab
 echo '@reboot root /usr/bin/chkrootkit' | tee -a /etc/crontab
