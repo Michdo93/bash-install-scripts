4ccfade0605c3e58a5f0b39050633d6e8154e11e
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-19 22:29:17 +0100

    Update install_virtualbox.bash

--- install_virtualbox.bash
+++ install_virtualbox.bash
@@ -4,13 +4,13 @@ sudo apt upgrade -y
 
 sudo apt install curl git wget net-tools -y
 
-sudo apt-get install virtualbox virtualbox-qt virtualbox-dkms -y
+sudo apt install virtualbox virtualbox-qt virtualbox-dkms -y
 
 sudo adduser $USER vboxusers
 newgrp - vboxusers
 
-sudo apt-get install virtualbox-guest-additions-iso -y
+sudo apt install virtualbox-guest-additions-iso -y
 
-sudo apt-get install virtualbox-guest-x11 -y
+sudo apt install virtualbox-guest-x11 -y
 
-sudo apt-get install virtualbox-ext-pack -y
+sudo apt install virtualbox-ext-pack -y
