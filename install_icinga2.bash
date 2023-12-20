eb843c4275d5488e8c68d27b3870948fd42b75a1
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 14:26:51 +0100

    Update install_icinga2.bash

--- install_icinga2.bash
+++ install_icinga2.bash
@@ -20,10 +20,34 @@ apply_changes() {
     echo "Änderungen in $1 angewendet."
 }
 
-sudo apt update
-sudo apt upgrade -y
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
 
-sudo apt install curl git wget net-tools -y
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# Aktualisieren und Upgraden
+sudo_available=$(check_sudo)
+run_command "apt update" "$sudo_available"
+run_command "apt upgrade -y" "$sudo_available"
+
+# Installieren von Paketen
+run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
 sudo apt install apache2 mariadb-server mariadb-client mariadb-common php php-gd php-mbstring php-mysqlnd php-curl php-xml php-cli php-soap php-intl php-xmlrpc php-zip php-common php-opcache php-gmp php-imagick php-pgsql -y
 
