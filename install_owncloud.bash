27f07e9d16f3f4f3f1c5d88bcdf66ab6b5a3773d
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 15:54:41 +0100

    Update install_owncloud.bash

--- install_owncloud.bash
+++ install_owncloud.bash
@@ -33,19 +33,19 @@ run_command "apt install curl git wget net-tools -y" "$sudo_available"
 host_name=$(hostname)
 
 # Abhängigkeiten installieren
-sudo apt install -y apache2 mariadb-server libapache2-mod-php php7.4-gd php7.4-json php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl php7.4-mcrypt php-imagick php7.4-xml php7.4-zip unzip -y
+run_command "apt install -y apache2 mariadb-server libapache2-mod-php php7.4-gd php7.4-json php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl php7.4-mcrypt php-imagick php7.4-xml php7.4-zip unzip -y" "$sudo_available"
 
 ./install_mariadb.bash
 
 # OwnCloud herunterladen und extrahieren
 wget https://download.owncloud.org/community/owncloud-complete-20210510.zip
 unzip owncloud-complete-20210510.zip
-sudo mv owncloud /var/www/html/
-sudo chown -R www-data:www-data /var/www/html/owncloud/
-sudo chmod -R 755 /var/www/html/owncloud/
+run_command "mv owncloud /var/www/html/" "$sudo_available"
+run_command "chown -R www-data:www-data /var/www/html/owncloud/" "$sudo_available"
+run_command "chmod -R 755 /var/www/html/owncloud/" "$sudo_available"
 
 # Apache VirtualHost-Konfiguration erstellen
-cat <<EOL | sudo tee /etc/apache2/sites-available/owncloud.conf
+cat <<EOL | run_command "tee /etc/apache2/sites-available/owncloud.conf" "$sudo_available"
 <VirtualHost *:80>
     ServerAdmin webmaster@$host_name
     DocumentRoot /var/www/html/owncloud
@@ -66,20 +66,15 @@ cat <<EOL | sudo tee /etc/apache2/sites-available/owncloud.conf
 EOL
 
 # Apache-Konfigurationen aktivieren
-sudo a2ensite owncloud.conf
-sudo a2enmod rewrite
-sudo systemctl restart apache2
+run_command "a2ensite owncloud.conf" "$sudo_available"
+run_command "a2enmod rewrite" "$sudo_available"
+run_command "systemctl restart apache2" "$sudo_available"
 
 # OwnCloud abschließen
-sudo -u www-data php /var/www/html/owncloud/occ maintenance:install --database "mysql" --database-name "owncloud"  --database-user "root" --database-pass "password" --admin-user "admin" --admin-pass "adminpassword"
-
-# Optional: Redis als Cache für OwnCloud aktivieren (erfordert Redis-Server-Installation)
-# sudo apt install -y redis-server
-# sudo -u www-data php /var/www/html/owncloud/occ config:system:set memcache.local --value '\OC\Memcache\Redis'
-# sudo -u www-data php /var/www/html/owncloud/occ config:system:set redis --value '{"host": "localhost", "port": "6379"}'
+run_command "-u www-data php /var/www/html/owncloud/occ maintenance:install --database \"mysql\" --database-name \"owncloud\"  --database-user \"root\" --database-pass \"password\" --admin-user \"admin\" --admin-pass \"adminpassword\"" "$sudo_available"
 
 # Systemd-Dienst für OwnCloud erstellen
-cat <<EOL | sudo tee /etc/systemd/system/owncloud.service
+cat <<EOL | run_command "tee /etc/systemd/system/owncloud.service" "$sudo_available"
 [Unit]
 Description=OwnCloud
 After=network.target
@@ -95,5 +90,5 @@ WantedBy=multi-user.target
 EOL
 
 # Systemd-Dienst aktivieren und starten
-sudo systemctl enable owncloud.service
-sudo systemctl start owncloud.service
+run_command "systemctl enable owncloud.service" "$sudo_available"
+run_command "systemctl start owncloud.service" "$sudo_available"
