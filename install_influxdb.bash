c90d4529814637eb52ad2759b55d65290a19de57
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 15:27:26 +0100

    Update install_influxdb.bash

--- install_influxdb.bash
+++ install_influxdb.bash
@@ -33,8 +33,8 @@ wget -q https://repos.influxdata.com/influxdata-archive_compat.key
 echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
 echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 
-sudo apt update
-sudo apt install influxdb2 -y
+run_command "apt update" "$sudo_available"
+run_command "apt install influxdb2 -y" "$sudo_available"
 
-sudo systemctl start influxdb.service
-sudo systemctl enable influxdb.service
+run_command "systemctl start influxdb.service" "$sudo_available"
+run_command "systemctl enable influxdb.service" "$sudo_available"
