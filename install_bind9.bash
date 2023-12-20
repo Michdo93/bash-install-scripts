9e16ba540a20ff4189fee46f2386a45bb431ad35
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 15:01:00 +0100

    Update install_bind9.bash

--- install_bind9.bash
+++ install_bind9.bash
@@ -29,7 +29,7 @@ run_command "apt upgrade -y" "$sudo_available"
 # Installieren von Paketen
 run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
-sudo apt install bind9 -y
+run_command "apt install bind9 -y" "$sudo_available"
 
-sudo systemctl start bind9.service
-sudo systemctl enable bind9.service
+run_command "systemctl start bind9.service" "$sudo_available"
+run_command "systemctl enable bind9.service" "$sudo_available"
