2078edf3d0180a0a0409245792681f0dea31bf99
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 15:11:00 +0100

    Update install_docker.bash

--- install_docker.bash
+++ install_docker.bash
@@ -30,25 +30,25 @@ run_command "apt upgrade -y" "$sudo_available"
 run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl gnupg -y
-sudo install -m 0755 -d /etc/apt/keyrings
+run_command "apt update" "$sudo_available"
+run_command "apt install ca-certificates curl gnupg -y" "$sudo_available"
+run_command "install -m 0755 -d /etc/apt/keyrings" "$sudo_available"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+run_command "chmod a+r /etc/apt/keyrings/docker.gpg" "$sudo_available"
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+  run_command "tee /etc/apt/sources.list.d/docker.list > /dev/null" "$sudo_available"
+run_command "apt update" "$sudo_available"
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+run_command "apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y" "$sudo_available"
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
+run_command "groupadd docker" "$sudo_available"
+run_command "usermod -aG docker $USER" "$sudo_available"
 
-sudo systemctl start docker.service
-sudo systemctl enable docker.service
-sudo systemctl start containerd.service
-sudo systemctl enable containerd.service
+run_command "systemctl start docker.service" "$sudo_available"
+run_command "systemctl enable docker.service" "$sudo_available"
+run_command "systemctl start containerd.service" "$sudo_available"
+run_command "systemctl enable containerd.service" "$sudo_available"
