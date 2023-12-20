cf97a3d0aeeacace2c63148ac79c83c2be3f07ce
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 14:30:23 +0100

    Update and rename install_nfs.bash to install_nfs-kernel-server.bash

--- install_nfs.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt upgrade -y
-
-sudo apt install curl git wget net-tools -y
-
-sudo apt install nfs-kernel-server -y
-sudo systemctl start nfs-kernel-server.service
-sudo systemctl enable nfs-kernel-server.service
