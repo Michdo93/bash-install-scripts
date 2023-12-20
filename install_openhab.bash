368aff499633039992b145b8ace6a558d0377cf1
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 15:50:16 +0100

    Update install_openhab.bash

--- install_openhab.bash
+++ install_openhab.bash
@@ -30,29 +30,29 @@ run_command "apt upgrade -y" "$sudo_available"
 run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
 # Install Java
-sudo apt install openjdk-17-jdk openjdk-17-jre -y
+run_command "apt install openjdk-17-jdk openjdk-17-jre -y" "$sudo_available"
 
 # Setze JAVA_HOME
 JAVA_HOME="/usr/lib/jvm/jdk-17"
-echo "JAVA_HOME=\"$JAVA_HOME\"" | sudo tee -a /etc/environment
+echo "JAVA_HOME=\"$JAVA_HOME\"" | run_command "tee -a /etc/environment" "$sudo_available"
 
 # Bearbeite die PATH-Zeile in /etc/environment
-sudo sed -i "/^PATH=/ s/\"$/:$JAVA_HOME\/bin\"/" /etc/environment
+run_command "sed -i "/^PATH=/ s/\"$/:$JAVA_HOME\/bin\"/" /etc/environment" "$sudo_available"
 
 # Aktualisiere die Umgebungsvariablen
 source /etc/environment
 
 # Install openHAB
 curl -fsSL "https://openhab.jfrog.io/artifactory/api/gpg/key/public" | gpg --dearmor > openhab.gpg
-sudo mkdir /usr/share/keyrings
-sudo mv openhab.gpg /usr/share/keyrings
-sudo chmod u=rw,g=r,o=r /usr/share/keyrings/openhab.gpg
+run_command "mkdir /usr/share/keyrings" "$sudo_available"
+run_command "mv openhab.gpg /usr/share/keyrings" "$sudo_available"
+run_command "chmod u=rw,g=r,o=r /usr/share/keyrings/openhab.gpg" "$sudo_available"
 
-echo 'deb [signed-by=/usr/share/keyrings/openhab.gpg] https://openhab.jfrog.io/artifactory/openhab-linuxpkg stable main' | sudo tee /etc/apt/sources.list.d/openhab.list
+echo 'deb [signed-by=/usr/share/keyrings/openhab.gpg] https://openhab.jfrog.io/artifactory/openhab-linuxpkg stable main' | run_command "tee /etc/apt/sources.list.d/openhab.list" "$sudo_available"
 
-sudo apt update
+run_command "apt update" "$sudo_available"
 
-sudo apt install openhab openhab-addons -y
+run_command "apt install openhab openhab-addons -y" "$sudo_available"
 
-sudo systemctl start openhab.service
-sudo systemctl enable openhab.service
+run_command "systemctl start openhab.service" "$sudo_available"
+run_command "systemctl enable openhab.service" "$sudo_available"
