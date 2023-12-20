ecaab13733757af822c8643220701bb3d2b5afc5
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 14:35:19 +0100

    Update and rename install_ros_melodic.bash to install_ros-melodic.bash

--- install_ros-melodic.bash
+++ install_ros-melodic.bash
@@ -1,9 +1,33 @@
 #!/bin/bash
-# Apache License 2.0
-# Copyright (c) 2018, ROBOTIS CO., LTD.
 
-sudo apt update
-sudo apt upgrade -y
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# Aktualisieren und Upgraden
+sudo_available=$(check_sudo)
+run_command "apt update" "$sudo_available"
+run_command "apt upgrade -y" "$sudo_available"
+
+# Installieren von Paketen
+run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
 echo ""
 echo "[Note] Target OS version  >>> Ubuntu 18.04.x (Bionic Beaver) or Linux Mint 19.x"
