d825e1616f61fefd950ce8cc55b6239414f3243b
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 16:26:57 +0100

    Update install_vscode.bash

--- install_vscode.bash
+++ install_vscode.bash
@@ -29,12 +29,12 @@ run_command "apt upgrade -y" "$sudo_available"
 # Installieren von Paketen
 run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
-sudo apt install wget gpg -y
+run_command "apt install wget gpg -y" "$sudo_available"
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+run_command "install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg" "$sudo_available"
+run_command "sh -c 'echo \"deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main\" > /etc/apt/sources.list.d/vscode.list'" "$sudo_available"
 rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https -y
-sudo apt update
-sudo apt install code -y
+run_command "apt install apt-transport-https -y" "$sudo_available"
+run_command "apt update" "$sudo_available"
+run_command "apt install code -y" "$sudo_available"
