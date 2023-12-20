37e6acbe930a835738af6616738ea01568ebbbcb
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 14:36:15 +0100

    Update install_speedtest.bash

--- install_speedtest.bash
+++ install_speedtest.bash
@@ -1,8 +1,34 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade -y
 
-sudo apt install curl git wget net-tools -y
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# Aktualisieren und Upgraden
+sudo_available=$(check_sudo)
+run_command "apt update" "$sudo_available"
+run_command "apt upgrade -y" "$sudo_available"
+
+# Installieren von Paketen
+run_command "apt install curl git wget net-tools -y" "$sudo_available"
+
 sudo apt install apt-transport-https gnupg1 dirmngr lsb-release -y
 
 curl -L https://packagecloud.io/ookla/speedtest-cli/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/speedtestcli-archive-keyring.gpg >/dev/null
