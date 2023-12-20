a2489e5fea6f5644324292dbe845858f3e45ba5d
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-20 14:40:59 +0100

    Update and rename release_upgrade.bash to do-release-upgrade.bash

---
+++ do-release-upgrade.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# Aktualisieren und Upgraden
+sudo_available=$(check_sudo)
+run_command "apt update" "$sudo_available"
+run_command "apt upgrade -y" "$sudo_available"
+run_command "apt dist-upgrade -y" "$sudo_available"
+run_command "do-release-upgrade -f DistUpgradeViewNonInteractive" "$sudo_available"
