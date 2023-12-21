bf691dbf672af6a6cc93ff44fc8399be8dca4cda
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-21 20:04:37 +0100

    Update nextcloud.bash

--- nextcloud.bash
+++ nextcloud.bash
@@ -31,7 +31,20 @@ run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
 run_command "apt install apache2 mariadb-server libapache2-mod-php php-gd php-mysql php-curl php-mbstring php-intl php-gmp php-bcmath php-xml php-imagick php-zip -y" "$sudo_available"
 
-./install_mysql.bash
+# Abhängigkeiten installieren
+run_command "apt install -y apache2 libapache2-mod-php php7.4-gd php7.4-json php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl php7.4-mcrypt php-imagick php7.4-xml php7.4-zip unzip -y" "$sudo_available"
+
+# Überprüfen, ob MariaDB bereits installiert ist
+if ! command -v mariadb &> /dev/null; then
+    # Installieren von Paketen
+    run_command "apt install mariadb-server mariadb-client mariadb-common -y" "$sudo_available"
+
+    # Durchführen der MariaDB-Sicherheitsinstallation
+    echo -e "mariadb_root\nmariadb_root\nY\nn\nY\nY\nY\n" | run_command "mysql_secure_installation" "$sudo_available"
+
+    run_command "systemctl start mariadb.service" "$sudo_available"
+    run_command "systemctl enable mariadb.service" "$sudo_available"
+fi
 
 # Nextcloud herunterladen und extrahieren
 wget https://download.nextcloud.com/server/releases/latest.tar.bz2
@@ -70,7 +83,7 @@ run_command "a2enmod rewrite" "$sudo_available"
 run_command "systemctl restart apache2" "$sudo_available"
 
 # Nextcloud abschließen
-run_command "-u www-data php /var/www/html/nextcloud/occ maintenance:install --database \"mysql\" --database-name \"nextcloud\" --database-user \"root\" --database-pass \"password\" --admin-user \"admin\" --admin-pass \"adminpassword\"" "$sudo_available"
+run_command "-u www-data php /var/www/html/nextcloud/occ maintenance:install --database \"mysql\" --database-name \"nextcloud\" --database-user \"nextcloud\" --database-pass \"nextcloud\" --admin-user \"mariadb_root\" --admin-pass \"mariadb_root\"" "$sudo_available"
 
 # Systemd-Dienst für Nextcloud erstellen
 cat <<EOL | run_command "tee /etc/systemd/system/nextcloud.service" "$sudo_available"
