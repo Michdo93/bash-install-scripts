d315bba51c2648c2b784f05f67e0a2278bd69c3b
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-21 21:11:58 +0100

    Update and rename influxdb.bash to influxdb2.bash

--- influxdb2.bash
+++ influxdb2.bash
@@ -38,3 +38,11 @@ run_command "apt install influxdb2 -y" "$sudo_available"
 
 run_command "systemctl start influxdb.service" "$sudo_available"
 run_command "systemctl enable influxdb.service" "$sudo_available"
+
+# Setze die InfluxDB-Zugangsdaten
+INFLUXDB_HOST="localhost"
+INFLUXDB_PORT="8086"
+INFLUXDB_USERNAME="admin"
+INFLUXDB_PASSWORD="influxdb"
+
+influx -host "$INFLUXDB_HOST" -port "$INFLUXDB_PORT" -token "$INFLUXDB_USERNAME:$INFLUXDB_PASSWORD" -organization "influxdb" -execute "CREATE USER $INFLUXDB_USERNAME WITH PASSWORD '$INFLUXDB_PASSWORD' WITH ALL PRIVILEGES"
