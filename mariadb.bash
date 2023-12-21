29f4cf2c089edd76bb4cdff55d88cfe24e8ce4f1
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-21 15:58:16 +0100

    Update mariadb.bash

--- mariadb.bash
+++ mariadb.bash
@@ -31,7 +31,7 @@ run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
 run_command "apt install mariadb-server -y" "$sudo_available"
 
-run_command "mysql_secure_installation" "$sudo_available"
+echo -e "mariadb_root\nmariadb_root\nY\nn\nY\nY\nY\n" | run_command "mysql_secure_installation" "$sudo_available"
 
 run_command "systemctl start mariadb.service" "$sudo_available"
 run_command "systemctl enable mariadb.service" "$sudo_available"
