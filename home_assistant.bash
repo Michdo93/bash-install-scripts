fc4ed617f1a7884ca701e709fafc3c514315a26a
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-21 14:15:49 +0100

    Update home_assistant.bash

--- home_assistant.bash
+++ home_assistant.bash
@@ -29,28 +29,41 @@ run_command "apt upgrade -y" "$sudo_available"
 # Installieren von Paketen
 run_command "apt install curl git wget net-tools -y" "$sudo_available"
 
-./install_python.bash
+run_command "apt install python3-pip -y" "$sudo_available"
 
-# As root
+# Installieren von Python-Entwicklungs- und Build-Abhängigkeiten
+run_command "apt install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev" "$sudo_available"
+
+# Als root fortfahren
 run_command "su" "$sudo_available"
+
+# Aktualisiere Paketliste
 apt update
+
+# Installiere erforderliche Pakete
 apt install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 
-# Switch to Home Assistant User
+# Zum Home Assistant-Benutzer wechseln
 run_command "su - homeassistant" "$sudo_available"
+
+# Wechsle zum Home Assistant-Verzeichnis und erstelle ein virtuelles Umgebung
 cd /home/homeassistant/
 python3 -m venv homeassistant_venv
 source /home/homeassistant/homeassistant_venv/bin/activate
 pip install --upgrade pip
+
+# Herunterladen der Anforderungen
 wget https://raw.githubusercontent.com/home-assistant/home-assistant/master/requirements_all.txt -O requirements_all.txt
-# This takes hours to finish and you may need to install additional failed dependencies if you get errors
+
+# Installation der Anforderungen (dies kann Stunden dauern und Sie müssen möglicherweise fehlgeschlagene Abhängigkeiten manuell installieren)
 pip install -r requirements_all.txt
 pip install mysqlclient
 pip install homeassistant
-# Go back to root
+
+# Zurück zu root wechseln
 exit
 
-# edit systemd service unit for new virtualenv
+# Bearbeiten der systemd-Service-Datei für das neue Virtualenv
 cat <<EOL | run_command "tee /etc/systemd/system/home-assistant.service" "$sudo_available"
 [Unit]
 Description=Home Assistant
@@ -65,5 +78,6 @@ ExecStart=/home/homeassistant/homeassistant_venv/bin/hass -c "/home/homeassistan
 WantedBy=multi-user.target
 EOL
 
+# systemd-Service aktivieren und starten
 run_command "systemctl start home-assistant.service" "$sudo_available"
 run_command "systemctl enable home-assistant.service" "$sudo_available"
