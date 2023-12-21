6bf31969d504b597c6403f418c40e71b1f778cc9
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-21 12:03:15 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -3,6 +3,13 @@
 # Aktuelles Verzeichnis
 skriptVerzeichnis="."
 
+# Arrays für die verschiedenen Skripte
+vorherigeSkripte=()
+ufw=()
+ufw_with_ports=()
+aide=()
+do_release_upgrade=()
+
 # Array mit den Namen der Skripte im Verzeichnis
 skriptArray=("$skriptVerzeichnis/"*)
 
@@ -20,12 +27,33 @@ for skript in "${skriptArray[@]}"; do
     read -n 1 markierung
     echo
     if [ "$markierung" == "Y" ] || [ "$markierung" == "y" ]; then
-        ausgewaehlteSkripte+=("$skript")
+        case "$(basename "$skript")" in
+            "do-release-upgrade.bash")
+                do_release_upgrade=("$skript")
+                ;;
+            "aide.bash")
+                aide=("$skript" "${aide[@]}")
+                ;;
+            "ufw.bash")
+                # Überprüfen, ob ufw_with_ports.bash bereits ausgewählt wurde
+                if [[ ! " ${ufw_with_ports[@]} " =~ " ${skript} " ]]; then
+                    ufw=("$skript" "${ufw[@]}")
+                fi
+                ;;
+            "ufw-with-ports.bash")
+                ufw_with_ports=("$skript" "${ufw_with_ports[@]}")
+                # Ufw.bash aus vorherigeSkripte entfernen
+                vorherigeSkripte=("${vorherigeSkripte[@]/ufw.bash}")
+                ;;
+            *)
+                vorherigeSkripte+=("$skript")
+                ;;
+        esac
     fi
 done
 
-# Ausführung der ausgewählten Skripte
-for skript in "${ausgewaehlteSkripte[@]}"; do
+# Ausführung der ausgewählten Skripte in gewünschter Reihenfolge
+for skript in "${vorherigeSkripte[@]}" "${ufw_with_ports[@]}" "${ufw[@]}" "${aide[@]}" "${do_release_upgrade[@]}"; do
     echo "Führe Skript aus: $skript"
     bash "$skript"
 done
