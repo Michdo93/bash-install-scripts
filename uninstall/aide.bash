84356c0310578bc8f94ee579ed5d8f8f1d7e5491
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-22 12:26:36 +0100

    Create aide.bash

---
+++ uninstall/aide.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# Löschen des täglichen Cron-Jobs für AIDE
+cronjob_line="0 0 * * * /usr/sbin/aide --check"
+(crontab -l | grep -v "$cronjob_line") | crontab -
+
+# Optionale Deinstallation von aide-Paket
+run_command "apt purge aide -y" "$(check_sudo)"
