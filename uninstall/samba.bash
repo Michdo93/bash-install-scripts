3d6ec7dc62d1f3681c58770ebe3aacc596d628f3
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-22 14:26:44 +0100

    Create samba.bash

---
+++ uninstall/samba.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# Aktualisieren und Upgraden
+sudo_available=$(check_sudo)
+run_command "apt update" "$sudo_available"
+run_command "apt upgrade -y" "$sudo_available"
+
+# Installieren von Paketen
+run_command "apt install curl git wget net-tools -y" "$sudo_available"
+run_command "apt install samba samba-common-bin -y" "$sudo_available"
+
+# Starten und Aktivieren des Samba-Dienstes
+run_command "systemctl start smbd.service" "$sudo_available"
+run_command "systemctl enable smbd.service" "$sudo_available"
+
+echo "Samba wurde erfolgreich installiert und gestartet."
