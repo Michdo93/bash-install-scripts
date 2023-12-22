476a4005d5b38a5d1a304927f83b8a1189c4b483
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-22 14:08:57 +0100

    Create open-vm-tools.bash

---
+++ uninstall/open-vm-tools.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# VMware Tools deinstallieren
+run_command "apt purge open-vm-tools open-vm-tools-desktop -y" "$sudo_available"
+run_command "apt autoremove -y" "$sudo_available"
+run_command "apt clean" "$sudo_available"
