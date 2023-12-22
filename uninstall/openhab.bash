9e98fe9ae79bb3e5c1e1ad05c9b14f0088a47f08
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-22 14:09:46 +0100

    Create openhab.bash

---
+++ uninstall/openhab.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# openHAB deinstallieren
+run_command "systemctl stop openhab.service" "$sudo_available"
+run_command "systemctl disable openhab.service" "$sudo_available"
+run_command "apt purge openhab openhab-addons -y" "$sudo_available"
+run_command "apt autoremove -y" "$sudo_available"
+run_command "apt clean" "$sudo_available"
+
+# Entfernen der openHAB-Repository-Konfiguration
+run_command "rm /etc/apt/sources.list.d/openhab.list" "$sudo_available"
+run_command "rm /usr/share/keyrings/openhab.gpg" "$sudo_available"
