b08821efff8beb2e78a3e5bbccfc452aa1c699e2
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-22 14:21:26 +0100

    Create rkhunter.bash

---
+++ uninstall/rkhunter.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# Aktualisieren und Upgraden
+sudo_available=$(check_sudo)
+run_command "apt update" "$sudo_available"
+run_command "apt upgrade -y" "$sudo_available"
+
+# Installieren von Paketen
+run_command "apt install curl git wget net-tools -y" "$sudo_available"
+
+# Installieren von Rootkit Hunter
+run_command "apt install rkhunter -y" "$sudo_available"
+run_command "apt install mailutils -y" "$sudo_available"
+
+# Einrichten des täglichen Cron-Jobs für rkhunter
+echo '10 3 * * * root /usr/bin/rkhunter --cronjob --update' | run_command "tee -a /etc/crontab" "$sudo_available"
+echo '@reboot root /usr/bin/rkhunter -c --cronjob --update' | run_command "tee -a /etc/crontab" "$sudo_available"
+
+echo "Rootkit Hunter (rkhunter) wurde erfolgreich installiert und konfiguriert."
