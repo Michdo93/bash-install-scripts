ddcf7b5da0325b7da8466e4ac5bae10d18e0a6df
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-22 12:51:15 +0100

    Create habapp.bash

---
+++ uninstall/habapp.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# HABApp-Dienst deaktivieren und stoppen
+run_command "systemctl stop habapp.service" "$(check_sudo)"
+run_command "systemctl disable habapp.service" "$(check_sudo)"
+
+# HABApp Virtual Environment und Installationsverzeichnis löschen
+HABAPP_DIR="/opt/habapp"
+
+run_command "rm -rf $HABAPP_DIR" "$(check_sudo)"
+run_command "rm -rf /etc/openhab/habapp" "$(check_sudo)"
+
+# HABApp systemd-Service-Datei löschen
+run_command "rm -f /etc/systemd/system/habapp.service" "$(check_sudo)"
