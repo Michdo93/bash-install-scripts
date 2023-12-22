463d893224499535efdc6657a9942a10ac886162
Michdo93 <michaeldoerflinger93@gmail.com>
2023-12-22 12:40:49 +0100

    Create dropbear.bash

---
+++ uninstall/dropbear.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo "$cmd"
+    else
+        "$cmd"
+    fi
+}
+
+# Stoppen und Deaktivieren von Dropbear-Diensten (falls gestartet)
+run_command "systemctl stop dropbear.service" "$(check_sudo)"
+run_command "systemctl disable dropbear.service" "$(check_sudo)"
+
+# Optionale Deinstallation von Dropbear
+run_command "apt purge dropbear -y" "$(check_sudo)"
