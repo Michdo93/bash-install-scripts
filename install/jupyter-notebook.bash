bb704b768d15a209248f1994b14eb605f88d8285
Michdo93 <michaeldoerflinger93@gmail.com>
2024-01-12 11:16:38 +0100

    Create jupyter-notebook.bash

---
+++ install/jupyter-notebook.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Funktion, um zu prüfen, ob sudo verfügbar ist
+check_sudo() {
+    if command -v sudo &> /dev/null; then
+        echo "sudo"
+    else
+        echo ""
+    fi
+}
+
+# Funktion zum Ausführen von Befehlen mit oder ohne sudo
+run_command() {
+    local cmd="$1"
+    local sudo_available="$2"
+
+    if [ -n "$sudo_available" ]; then
+        sudo $cmd
+    else
+        $cmd
+    fi
+}
+
+# Aktualisieren und Upgraden
+sudo_available=$(check_sudo)
+run_command "apt update" "$sudo_available"
+run_command "apt upgrade -y" "$sudo_available"
+
+# Installieren von Paketen
+run_command "apt install curl git wget net-tools -y" "$sudo_available"
+
+python3 -m pip install jupyter notebook
